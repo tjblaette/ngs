b1cdf299e52cb95d733bc66b750dabb4dba55d98
Tamara <tamara.blaette@uni-ulm.de>
2015-09-18 16:45:18 +0200

    Added plotting of normalized gene counts to deseq Skript, for now all genes are plotted to a simple pdf (very large...)

--- deseq2.R
+++ deseq2.R
@@ -21,10 +21,9 @@ fromFile <- function(input) {
 }
 
 
-
-
 mydds <- fromFile(input_file)
 myresults <- results(mydds, alpha=my_alpha, altHypothesis="greaterAbs", lfcThreshold=my_lfc)
+
 
 #sort according to adjusted p-value
 myresultsOrdered <- myresults[order(myresults$padj),]
@@ -33,10 +32,8 @@ myresultsOrdered <- myresults[order(myresults$padj),]
 rownames(myresultsOrdered) <- unlist(strsplit(rownames(myresultsOrdered), split='\\.'))[2*(1:length(rownames(myresultsOrdered)))-1]
 
 
-
 #system(paste("head -1", f," > blub_sorted"))
 #system(paste("tail -n +2", f," | sort >> blub_sorted"))
-
 
 
 ##This annotation gives different results from ENSEMBLE's biomart or browser queries so I am not using it!
@@ -49,6 +46,14 @@ rownames(myresultsOrdered) <- unlist(strsplit(rownames(myresultsOrdered), split=
 sink(paste(input_file,"_DESeq2results_summary.txt",sep=""))
 summary(myresultsOrdered, alpha=my_alpha)
 sink()
+
+#plot gene counts to pdf
+pdf(paste(input_file,"_DESeq2results_geneCountPlots.pdf",sep=""))
+  for (i in 1:(dim(mydds)[1]))
+  {
+    plotCounts(mydds, gene=i, intgroup="condition")
+  }
+dev.off()
 
 #write deg analysis results to file
 write.table(myresultsOrdered, file=paste(input_file,"_DESeq2results.txt",sep=""))
