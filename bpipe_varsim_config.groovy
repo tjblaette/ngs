822dab70b299aedd71e43be448ad81853e988c0b
Tamara <tamara.blaette@uni-ulm.de>
2015-09-21 09:16:44 +0200

    Added GATK known_sites to varsim pipeline

--- bpipe_varsim_config.groovy
+++ bpipe_varsim_config.groovy
@@ -6,6 +6,8 @@ FILTER="filter_exome_wFlankingSeq.sh"
 
 // KNOWN SEQUENCES
 REF="${NGS}/varsim_run/hs37d5.fa" //also includes unplaced/unlocalized contigs and alternative haplotypes
+GOLD_STANDARD_1000G_INDELS="${NGS}/known_sites/b37/Mills_and_1000G_gold_standard.indels.b37.vcf"
+PHASE1_1000G_INDELS="${NGS}/known_sites/b37/1000G_phase1.indels.b37.vcf"
 
 // TARGET SEQUENCES FOR COVERAGE CALC
 
