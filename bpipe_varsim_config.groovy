ea883eb7ea2f70810eaa585eebd6da15cb667bf9
Tamara <tamara.blaette@uni-ulm.de>
2015-09-23 09:23:26 +0200

    Added dbsnp file to varsim pipeline and removed filter script since variants are not filtered for VarSim

--- bpipe_varsim_config.groovy
+++ bpipe_varsim_config.groovy
@@ -2,12 +2,12 @@
 
 
 // VARIANT FILTER SCRIPT
-FILTER="filter_exome_wFlankingSeq.sh"
 
 // KNOWN SEQUENCES
 REF="${NGS}/varsim_run/hs37d5.fa" //also includes unplaced/unlocalized contigs and alternative haplotypes
 GOLD_STANDARD_1000G_INDELS="${NGS}/known_sites/b37/Mills_and_1000G_gold_standard.indels.b37.vcf"
 PHASE1_1000G_INDELS="${NGS}/known_sites/b37/1000G_phase1.indels.b37.vcf"
+DBSNP="${NGS}/known_sites/b37/dbsnp_138.b37.vcf"
 
 // TARGET SEQUENCES FOR COVERAGE CALC
 
