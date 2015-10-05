9000546be78a36814576d8d67c5b9f9fb3efd10e
Tamara <tamara.blaette@uni-ulm.de>
2015-10-05 16:06:58 +0200

    Finished pos calc for forward strand case 1

--- addCoords.sh
+++ addCoords.sh
@@ -8,17 +8,18 @@ FILE=$1
 # start with + strand, case1 (inter-mate gap within segment2):
 # Apppend: SS1 (Start segment1), LS1 (Length segment1), SS2a, LS2a, SS2b,LS2b, IMG (Inter-mate Gap)
 
-POS1_and_SS1="$(awk -v OFS='\t' '$14 ~ /p/ {print $0,$11}' "$FILE")"
-LS1="$( awk '$14 ~ /p/ {print $12}' "$FILE" | sed -e 's/[MDpN]/\t/g' -e 's/[0-9]\+[SHI]//g' | awk -v OFS='\t'  '{SUM=$1; for(i=2;i<=NF;i++)SUM=SUM+$i; print SUM}')"
-SS2a="$(awk '$14 ~ /p/ {print $13}' "$FILE")"
-LS2a="$(awk '$14 ~ /p/ {print $14}' "$FILE" | sed -e 's/[0-9-]\+p.*$//' -e 's/[MDN]/\t/g' -e 's/[0-9]\+[SHI]//g' | awk -v OFS='\t'  '{SUM=$1; for(i=2;i<=NF;i++)SUM=SUM+$i; print SUM}')"
-IMG="$(awk '$14 ~ /p/ {print $14}' "$FILE" | sed -e 's/.*[MIDNSH]\([0-9-]\+\)p.*$/\1/')"
-LS2b="$(awk '$14 ~ /p/ {print $14}' "$FILE" | sed -e 's/.*p\(.*\)$/\1/' -e 's/[MDN]/\t/g' -e 's/[0-9]\+[SHI]//g' | awk -v OFS='\t'  '{SUM=$1; for(i=2;i<=NF;i++)SUM=SUM+$i; print SUM}')"
+P1="$(awk -v OFS='\t' '$14 ~ /p/ {print $0}' "$FILE")"
+P1_SS1="$(awk -v OFS='\t' '$14 ~ /p/ {print $11}' "$FILE")"
+P1_LS1="$( awk '$14 ~ /p/ {print $12}' "$FILE" | sed -e 's/[MDpN]/\t/g' -e 's/[0-9]\+[SHI]//g' | awk -v OFS='\t'  '{SUM=$1; for(i=2;i<=NF;i++)SUM=SUM+$i; print SUM}')"
+P1_SS2a="$(awk '$14 ~ /p/ {print $13}' "$FILE")"
+P1_LS2a="$(awk '$14 ~ /p/ {print $14}' "$FILE" | sed -e 's/[0-9-]\+p.*$//' -e 's/[MDN]/\t/g' -e 's/[0-9]\+[SHI]//g' | awk -v OFS='\t'  '{SUM=$1; for(i=2;i<=NF;i++)SUM=SUM+$i; print SUM}')"
+P1_IMG="$(awk '$14 ~ /p/ {print $14}' "$FILE" | sed -e 's/.*[MIDNSH]\([0-9-]\+\)p.*$/\1/')"
+P1_LS2b="$(awk '$14 ~ /p/ {print $14}' "$FILE" | sed -e 's/.*p\(.*\)$/\1/' -e 's/[MDN]/\t/g' -e 's/[0-9]\+[SHI]//g' | awk -v OFS='\t'  '{SUM=$1; for(i=2;i<=NF;i++)SUM=SUM+$i; print SUM}')"
+
+P1_POSCOORDS="$(paste <(echo "$P1_SS1") <(echo "$P1_LS1") <(echo "$P1_SS2a") <(echo "$P1_LS2a") <(echo "$P1_IMG") <(echo "$P1_LS2b") | awk -v OFS='\t' '{print $1,$1+$2,$3,$3+$4,$3+$4+$5,$3+$4+$5+$6}')"
+
+#echo "$POSCOORDS" | head
+#head "coordsAdded.txt"
 
 
-#echo "$POS1_and_SS1" | head
-#echo "$LS1" | head 
 
-paste <(echo "$POS1_and_SS1") <(echo "$LS1") <(echo "$SS2a") <(echo "$LS2a") <(echo "$IMG") <(echo "$LS2b")> coordsAdded.txt
-
-head "coordsAdded.txt"
