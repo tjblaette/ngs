0a406411f69ee3c20796a942f98ba4bdf0952ea2
Tamara <tamara.blaette@uni-ulm.de>
2015-11-13 18:44:56 +0100

    Fixed hg19 GTF for RNAseq pipeline -> was GENCODE nomenclature for contigs, I now converted it to UCSC to match our reference

--- bpipe_rna_config.groovy
+++ bpipe_rna_config.groovy
@@ -6,11 +6,11 @@
 
 // KNOWN SEQUENCES
 REF="${NGS}/refgenome/GATK/ucsc.hg19.fasta" //also includes unplaced/unlocalized contigs and alternative haplotypes
-REFDIR="${NGS}/refgenome/GATK/starIndex/" //ref indexed by STAR with annotation
+REFDIR="${NGS}/refgenome/GATK/starIndexFixedGTF/" //ref indexed by STAR with annotation
 DBSNP="${NGS}/known_sites/hg19/dbsnp_138.hg19.vcf"
 GOLD_STANDARD_1000G_INDELS="${NGS}/known_sites/hg19/Mills_and_1000G_gold_standard.indels.hg19.sites.vcf"
 PHASE1_1000G_INDELS="${NGS}/known_sites/hg19/1000G_phase1.indels.hg19.sites.vcf"
-GTF="${NGS}/known_sites/hg19/gencode.v19.chr_patch_hapl_scaff.annotation.gtf"
+GTF="${NGS}/known_sites/hg19/gencode.v19.chr_patch_hapl_scaff.annotation_UCSCcontigs.gtf"
 
 // TARGET SEQUENCES FOR COVERAGE CALC
 TRUSEQ="${NGS}/refgenome/hg19/TruSeq-Exome-Targeted-Regions-BED-file.bed" 
