f93569b463807e9773646f9e13826d28dc7ceed0
Tamara <tamara.blaette@uni-ulm.de>
2016-01-20 15:42:30 +0100

    Added parameter to deseq2_wrapper.sh for ensembl-to-entrez ID conversion DB. This allows annotation for different organisms.

--- deseq2_wrapper.sh
+++ deseq2_wrapper.sh
@@ -3,13 +3,15 @@
 
 # $1 = input file for DESeq2
 # $2 = alpha for DEG statistic (default:0.1)
-# $3 = DB for gene id conversion
-# $4 = min absolute LFC tested for (default:0) -> does not take any effect because R function in deseq does not accept variable for lfc parameter
+# $3 = DB for gene id conversion (Ensembl to gene symbol)
+# $4 = DB for gene id conversion (Ensembl to Entrez gene ID)
+# $5 = min absolute LFC tested for (default:0) -> does not take any effect because R function in deseq does not accept variable for lfc parameter
 
 IN=$1
 ALPHA=${2:-0.1}
 BIOMART=${3:-'/NGS/known_sites/hg19/gencode.v19.chr_patch_hapl_scaff.annotation_UCSCcontigs.gtf'}   #was: /NGS/known_sites/human_ensembl_biomart_gene_ID_to_symbol/mart_export_sorted_woutLRG.txt'}
-LFC=${4:-0} # does not take any effect!
+ENTREZ=${4:-'/NGS/known_sites/hg19/biomart_ensembl74ID_to_entrezID_mapped.txt'}
+LFC=${5:-0} # does not take any effect!
 
 # check if the gene ID to Symbol conversion table exists already
 # if it does not, create it now
@@ -42,7 +44,6 @@ join -t '	' <(sed 's/\.[0-9]*//' "${BIOMART%.gtf}_geneIDtoSymbol.txt") ${IN}_DES
 grep -wv 'NA' ${IN}_DESeq2results_annotated.txt >  ${IN}_DESeq2results_annotated_woutNA.txt
 
 # annotate file with entrez gene IDs as well (for SPIA)
-ENTREZ="/NGS/known_sites/hg19/biomart_ensembl74ID_to_entrezID_mapped.txt"
 join -t '	' <( tail -n +2 ${IN}_DESeq2results_annotated_woutNA.txt) <(tail -n +2 $ENTREZ) > ${IN}_DESeq2results_annotated_woutNA_wEntrezIDs.txt
 
 
