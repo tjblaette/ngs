a69ccf2167ece364174a1996a91bec9fe520fe43
Tamara <tamara.blaette@uni-ulm.de>
2016-02-04 10:53:43 +0100

    Fixed coverage error: % on target is bp not reads (fixed string/column header) and summary script is now based on *.summary instead of *.txt files - this was necessary after changing coverage calculation in bpipe script to bedtools hist but apparently I had forgotten this back then

--- coverage_summary.sh
+++ coverage_summary.sh
@@ -12,10 +12,10 @@ fi
 
 
 #insert header
-echo 'File name,# reads on target,Average coverage,No coverage,1x Coverage,10x Coverage,15x Coverage,50x Coverage,120x Coverage,200x Coverage,500x Coverage,1000x Coverage,1500x Coverage,2000x Coverage,2500x Coverage' > ${dir}/coverage_summary.csv
+echo 'File name,bp on target,Average coverage,No coverage,1x Coverage,10x Coverage,15x Coverage,50x Coverage,120x Coverage,200x Coverage,500x Coverage,1000x Coverage,1500x Coverage,2000x Coverage,2500x Coverage' > ${dir}/coverage_summary.csv
 
 #extract coverage information from all BED.txt-files in $dir 
-for file in ${dir}/*BED.txt
+for file in ${dir}/*BED.summary
 do
 	echo -n "$(basename $file)," >> ${dir}/coverage_summary.csv
 	sed 's/.*(\(.*\))/\1/g' $file | sed 's/on average \(.*\) reads.*/\1/g' | tr '\n' ',' | sed 's/,$/\n/g' >> ${dir}/coverage_summary.csv 
