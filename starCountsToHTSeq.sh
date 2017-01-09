cd6727deffe242dc23653088e2e34ec1d7d5d3ab
Tamara <tamara.blaette@uni-ulm.de>
2017-01-09 12:46:11 +0100

    starCountsToHTSeq.sh: Fix spaces to tabs (copy-paste artefact)

--- starCountsToHTSeq.sh
+++ starCountsToHTSeq.sh
@@ -4,7 +4,7 @@
 # Default column to extract from STAR is 2 which corresponds to read counts for an unstranded protocol
 
  INPUT=$1
- COLUMN=${2:2} #for unstranded
+ COLUMN=${2:-2} #for unstranded
 
  OUTPUT=$(echo ${INPUT%_R1ReadsPerGene.out.tab}.counts)
  
@@ -13,4 +13,4 @@
  MULTIMAPPED=$(grep 'multimap' $INPUT | cut -f2) 
  UNMAPPED=$(grep 'unmapped' $INPUT | cut -f2)
 
-cat <(grep -v '^N_' $INPUT | sort | cut -f1-$COLUMN) <(echo "__no_feature$NOFEAT") <(echo "__ambiguous   $AMBIG") <(echo "__too_low_aQual   0") <(echo "__not_aligned $UNMAPPED") <(echo "__alignment_not_unique $MULTIMAPPED") > $OUTPUT
+cat <(grep -v '^N_' $INPUT | sort | cut -f1-$COLUMN) <(echo "__no_feature	$NOFEAT") <(echo "__ambiguous	$AMBIG") <(echo "__too_low_aQual	0") <(echo "__not_aligned	$UNMAPPED") <(echo "__alignment_not_unique	$MULTIMAPPED") > $OUTPUT
