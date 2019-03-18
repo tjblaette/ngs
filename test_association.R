95044a1a40cb3e429bdee83aff5e5983a8939449
tjblaette <tamara.blaette@uni-ulm.de>
2019-03-18 17:16:33 +0100

    test_association: allow 2 factors and remove gene/factor order restraint
    Before, one gene and one factor from the design tsv had to be passed.
    Now, two factors from the design may be given to test for association
    via FET. Also, gene and factor or factor and gene may be passed,
    regardless of the order, the same FET is performed.

--- test_association.R
+++ test_association.R
@@ -5,13 +5,13 @@ input_file <- args[1]
 normalized_counts_filename <- args[1] #"/media/data/tabl/pollack_ckaml/expression/p53Aberration_vs_wt/pollack_classLabels_p53mut_all_wBatch.tsv_DESeq2results_countsNormalized.txt"
 annotation_filename <- args[2] #"/media/data/tabl/pollack_ckaml/expression/p53Aberration_vs_wt/pollack_classLabels_p53mut_all_wBatch.tsv"
 
-gene <- args[3] #"CDKN1C"
-attribute <- args[4] #"TP53del"                                                                                                             
+attr1 <- args[3] #"CDKN1C"
+attr2 <- args[4] #"TP53del"
 
 
 counts <- read.table(
-        normalized_counts_filename, 
-        header=1, 
+        normalized_counts_filename,
+        header=1,
         check.names=FALSE,
         row.names=1)
 
@@ -19,30 +19,53 @@ gene_symbol <- counts$geneSymbol
 counts <- counts[,-1]
 
 anno <- read.table(
-        annotation_filename, 
+        annotation_filename,
         header=1,
         check.names=FALSE,
         row.names=1)
 
 counts <- counts[,rownames(anno)]
 
-gene_median <- median(t(counts[which(gene_symbol == gene),]))
+contingency_table <- ""
+if (attr1 %in% colnames(anno) && attr2 %in% colnames(anno)) {
+    print(unique(anno[[attr1]]))
+    contingency_table <- table(anno[c(attr1, attr2)]) #matrix(
+} else {
+    if ((attr1 %in% colnames(anno) && attr2 %in% gene_symbol) || (attr1 %in% gene_symbol && attr2 %in% colnames(anno))) {
+        if (attr1 %in% colnames(anno) && attr2 %in% gene_symbol) {
+            attribute <- attr1
+            gene <- attr2
+        } else {
+            if (attr2 %in% colnames(anno) && attr1 %in% gene_symbol) {
+            attribute <- attr2
+            gene <- attr1
+            }
+        }
 
-samples_high <- colnames(counts)[counts[which(gene_symbol == gene),] > gene_median]
-samples_low <- colnames(counts)[counts[which(gene_symbol == gene),] <= gene_median]
+        gene_median <- median(t(counts[which(gene_symbol == gene),]))
 
-attribute_in_samples_high <- table(anno[attribute][samples_high,])
-attribute_in_samples_low <- table(anno[attribute][samples_low,])
+        samples_high <- colnames(counts)[counts[which(gene_symbol == gene),] > gene_median]
+        samples_low <- colnames(counts)[counts[which(gene_symbol == gene),] <= gene_median]
+
+        attribute_in_samples_high <- table(anno[attribute][samples_high,])
+        attribute_in_samples_low <- table(anno[attribute][samples_low,])
+
+        #print(attribute_in_samples_low)
+        #print(attribute_in_samples_high)
 
 
-
-contingency_table <- matrix(
-        c(attribute_in_samples_high, attribute_in_samples_low),
-        nrow=2, 
-        ncol=2, 
-        dimnames=list(
-                attribute = names(attribute_in_samples_high),
-                expression = c(paste(gene,"high", sep="_"), paste(gene,"low", sep="_"))))
+        contingency_table <- matrix(
+                c(attribute_in_samples_high, attribute_in_samples_low),
+                nrow=2,
+                ncol=2,
+                dimnames=list(
+                        attribute = names(attribute_in_samples_high),
+                        expression = c(paste(gene,"high", sep="_"), paste(gene,"low", sep="_"))))
+    } else {
+        cat("\nAttribute not found in input files!\n\n")
+        quit()
+    }
+}
 
 
 #fisher.test(anno[[attribute]], as.factor(counts[which(gene_symbol == gene),] > gene_median))
@@ -55,7 +78,10 @@ cat("Inverse of Odds Ratio: ", 1 / fisher$estimate,"\n\n\n")
 cat("Contingency table:\n\n")
 print(contingency_table)
 
-cat("\n(The Odds Ratio is the ratio of the odds of a sample in group \"", rownames(contingency_table)[1], "\" being of group \"", colnames(contingency_table)[1], "\" vs \"", colnames(contingency_table)[2],"\")", sep="")
-cat("\n(The inverse Odds Ratio is the ratio of the odds of a sample in group \"", rownames(contingency_table)[1], "\" being of group \"", colnames(contingency_table)[2], "\" vs \"", colnames(contingency_table)[1],"\")", sep="")
-cat("\nThe median expression used to define high vs low expression was: ", gene_median, " normalized read counts\n\n", sep="")
+cat("\n(The Odds Ratio is the ratio of the odds of a sample in", dimnames(contingency_table)[[1]], "group \"", rownames(contingency_table)[1], "\" being of group \"", colnames(contingency_table)[1], "\" vs \"", colnames(contingency_table)[2],"\")", sep="")
+cat("\n(The inverse Odds Ratio is the ratio of the odds of a sample in group \"", rownames(contingency_table)[1], "\" being of group \"", colnames(contingency_table)[2], "\" vs \"", colnames(contingency_table)[1],"\")\n", sep="")
+
+if (exists("gene_median")) {
+    cat("\nThe median expression used to define high vs low expression was: ", gene_median, " normalized read counts\n\n", sep="")
+}
 
