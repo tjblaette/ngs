dc8cd7a07471c12fae15ad187238673a583d2477
Tamara J. Blätte <tamara.blaette@uni-ulm.de>
2019-03-21 22:03:14 -0700

    bamStat_summary.sh: Fix, clean code & document
    The wrong rows and columns were extracted,
    possibly because of the prev Picard update.

--- bamStat_summary.sh
+++ bamStat_summary.sh
@@ -1,23 +1,37 @@
 #!/bin/bash
 
-#$1 = directory containing Picards "*_metrics.txt" statistic files
+####
+# T.J.Blätte
+# 2015
+####
+#
+# Collects all of Picard's metric files from
+#       a specified folder, containing
+#       alignment and duplicate statistics,
+#       and saves them to a single TSV file.
+#
+# Args:
+#   DIR: Name of / Path to the folder to
+#        collect Picard files from.
+#
+# Output:
+#   $DIR/bamStat_summary.tsv: containing all
+#        of the collected and merged statistics.
+#
+####
 
-#when no input directory was supplied, assume "."
-if [[ $# -eq 0 ]] ;
-then
-        dir='.'
-else
-        dir=$1
-fi
+
+DIR=${1:-'.'}
+OUT="${DIR}/bamStat_summary.tsv"
 
 
-#insert header
-echo 'File name,# reads unmapped total,# reads mapped total,# paired reads mapped,# duplicate reads total,# paired duplicate reads,# paired optical duplicate reads,percent duplicate reads total' > ${dir}/bamStat_summary.csv
+# insert header
+echo -e "File name\t# reads unmapped total\t# reads mapped total\t# paired reads mapped\t# duplicate reads total\t# paired duplicate reads\t# paired optical duplicate reads\t#percent duplicate reads total" > "$OUT"
 
-#extract mapping and duplicate statistics from all metrics.txt-files in $dir 
-for file in ${dir}/*metrics.txt
+# extract mapping and duplicate statistics from all metrics.txt-files in $DIR
+for file in ${DIR}/*metrics.txt
 do
-        echo -n "$(basename $file)," >> ${dir}/bamStat_summary.csv
-	head  -9 $file | tail -1 | sed 's/[a-zA-Z]//g' | awk '{print $3,$1+2*$2,$2*2,$4+$5*2,$5*2,$6*2,$7}' >> ${dir}/bamStat_summary.csv
+        echo -ne "$(basename "$file")\t" >> "$OUT"
+        head  -n 8 "$file" | tail -n 1 | sed 's/[a-zA-Z]//g' | awk -v FS="\t" -v OFS="\t" '{print $5,$2+2*$3,2*$3,$6+2*$7,2*$7,2*$8,$9}' >> "$OUT"
 done
 
