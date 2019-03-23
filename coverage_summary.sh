f9c07bde6f5cd46aa7216039050a077b3e95d1b7
Tamara J. Blätte <tamara.blaette@uni-ulm.de>
2019-03-22 21:59:59 -0700

    coverage_summary.sh: Fix, clean code & document
    The "min 100x coverage" column header was missing.

--- coverage_summary.sh
+++ coverage_summary.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
 
-#$1 = directory containing statistic files
+####
+# T.J.Blätte
+# 2015
+####
+#
+# Collects all of the coverage stat summary
+#       files created using BEDTools
+#       from a specified folder and saves
+#       them to a single TSV file.
+#
+# Args:
+#   DIR: Name of / Path to the folder to
+#       collect coverage stat files from.
+#
+# Output:
+#   $DIR/coverage_summary.tsv: containing all
+#       of the collected and merged statistics.
+#
+####
 
-#when no input directory was supplied, assume "."
-if [[ $# -eq 0 ]] ;
-then
-        dir='.'
-else
-        dir=$1
-fi
+
+DIR=${1:-'.'}
+OUT="${DIR}/coverage_summary.tsv"
 
 
-#insert header
-echo 'File name,bp on target,Average coverage,No coverage,1x Coverage,10x Coverage,15x Coverage,50x Coverage,120x Coverage,200x Coverage,500x Coverage,1000x Coverage,1500x Coverage,2000x Coverage,2500x Coverage' > ${dir}/coverage_summary.csv
+# insert header
+echo -e "File name\tBp on target\tAverage coverage\tNo coverage\tMin 1x coverage\tMin 10x coverage\tMin 15x coverage\tMin 50x coverage\tMin 100x coverage\tMin 120x coverage\tMin 200x coverage\tMin 500x coverage\tMin 1000x coverage\tMin 1500x coverage\tMin 2000x coverage\tMin 2500x coverage" > "$OUT"
 
-#extract coverage information from all BED.txt-files in $dir 
-for file in ${dir}/*BED.summary
+# extract coverage information from all BEDTools coverage stat files in $DIR
+for file in ${DIR}/*BED.summary
 do
-	echo -n "$(basename $file)," >> ${dir}/coverage_summary.csv
-	sed 's/.*(\(.*\))/\1/g' $file | sed 's/on average \(.*\) reads.*/\1/g' | tr '\n' ',' | sed 's/,$/\n/g' >> ${dir}/coverage_summary.csv 
+        echo -ne "$(basename "$file")\t" >> "$OUT"
+        sed 's/.*(\(.*\))/\1/g' $file | sed 's/on average \(.*\) reads.*/\1/g' | tr '\n' '\t' | sed 's/\t$/\n/g' >> "$OUT"
 done
+
