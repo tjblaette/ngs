02d8db06c29e52580b408753ad375de120f9381c
Tamara J. Blätte <tamara.blaette@uni-ulm.de>
2019-03-22 22:38:56 -0700

    filter_summary.sh: Clean code & document

--- filter_summary.sh
+++ filter_summary.sh
@@ -1,24 +1,40 @@
 #!/bin/bash
 
-#$1 = directory containing statistic files
+####
+# T.J.Blätte
+# 2015
+####
+#
+# Collects all of the filtering stat files
+#       created by bpipe filter scripts
+#       from a specified folder, containing
+#       alignment and duplicate statistics,
+#       and saves them to a single TSV file.
+#
+# Args:
+#   DIR: Name of / Path to the folder to
+#       collect filtering stat files from.
+#
+# Output:
+#   $DIR/filter_summary.tsv: containing all
+#       of the collected and merged statistics.
+#
+####
 
-#when no input directory was supplied, assume "."
-if [[ $# -eq 0 ]] ;
-then
-        dir='.'
-else
-	dir=$1
-fi
 
-#insert header
-echo 'File name,Total calls,Exonic/Splicing,Nonsynonymous,Passed dbSNPi/COSMIC-filter,AML candidate genes' > ${dir}/filter_summary.txt
+DIR=${1:-'.'}
+OUT="${DIR}/filter_summary.tsv"
 
-#extract variant filtering statistics from all *stat*-files in $dir
-for file in ${dir}/*_filter_statistic.txt
+
+# insert header
+echo -e "File name\tTotal calls\tExonic/Splicing\tNonsynonymous\tPassed dbSNP/COSMIC-filter\tAML candidate genes" > "$OUT"
+
+# extract variant filtering statistics from all *stat*-files in $DIR
+for file in ${DIR}/*_filter_statistic.txt
 do
-        echo -n "$(basename ${file%_merged_filter_statistic.txt})," >> ${dir}/filter_summary.txt #file name
-	grep 'total' $file | sed 's/.*total of \(.*\) calls.*/\1/g' | tr '\n' ',' >> ${dir}/filter_summary.txt #total calls
-	grep 'total' $file | sed 's/\(.*\) out of.*/\1/g' | tr '\n' ',' >> ${dir}/filter_summary.txt #exonic/splicing variants in total
-	grep 'remain' $file | sed 's/\(.*\) calls.*/\1/g' | tr '\n' ',' >> ${dir}/filter_summary.txt #calls remaining after removal of synonymous variants and those failing dbSNP/COSMIC-filter
-	grep 'candidate' $file | sed 's/\(.*\) calls.*/\1/g' >> ${dir}/filter_summary.txt #calls affecting AML candidate genes
+        echo -ne "$(basename ${file%_merged_filter_statistic.txt})\t" >> "$OUT" #file name
+        grep 'total' "$file" | sed 's/.*total of \(.*\) calls.*/\1/g' | tr '\n' '\t' >> "$OUT" #total calls
+        grep 'total' "$file" | sed 's/\(.*\) out of.*/\1/g' | tr '\n' '\t' >> "$OUT" #exonic/splicing variants in total
+        grep 'remain' "$file" | sed 's/\(.*\) calls.*/\1/g' | tr '\n' '\t' >> "$OUT" #calls remaining after removal of synonymous variants and those failing dbSNP/COSMIC-filter
+        grep 'candidate' "$file" | sed 's/\(.*\) calls.*/\1/g' >> "$OUT" #calls affecting AML candidate genes
 done
